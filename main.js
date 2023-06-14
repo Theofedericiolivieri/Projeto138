318e45f2b3c0a28ca43b2448d68c485d19009484
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-06-14 19:58:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,9 +30,23 @@ ball = {
 
 function setup(){
   canvas =  createCanvas(700,550);
+  video=createCapture(VIDEO);
+  video.size(700,550);
+  poseNet=ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
 }
-
-
+function modelLoaded(){
+  console.log("Modelo Carregado!");
+}
+noseX=0;
+noseY=0;
+function gotPoses(results){
+  if(results.length>0){
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+    console.log("noseX = "+ noseX + " noseY = "+ noseY);
+  }
+}
 function draw(){
 
   background(0); 
